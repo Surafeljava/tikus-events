7872efa5dc64b114266af97445303a5d865488c9
Surafeljava <surafelk27@gmail.com>
2021-02-12 12:01:28 +0300

    creating the authentication structure

---
+++ lib/Presentation/AuthenticationUI/authentication.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Authentication Page!'),
+        ],
+      ),
+    );
+  }
+}
