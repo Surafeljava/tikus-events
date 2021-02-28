2a478887935c337776f0439f7160e0d5e5bc369c
danielteferadeti <56646941+danielteferadeti@users.noreply.github.com>
2021-02-28 12:32:42 +0300

    Update register_repository.dart
    Register repository Commited!

--- lib/registration/repository/register_repository.dart
+++ lib/registration/repository/register_repository.dart
@@ -3,8 +3,22 @@ import 'package:tikusevents/registration/data_provider/register_data_provider.da
 
 class RegisterRepository{
 
-  final RegisterDataProvider dataProvider;
-  RegisterRepository({this.dataProvider});
+  final RegisterDataProvider dataProvider;  
+   RegisterRepository({@required this.dataProvider}): assert(dataProvider != null);
 
+  Future<RegisterModel> createRegistor(RegisterModel register) async {
+    return await dataProvider.creaateRegistration(register);
+  }
 
+  Future<List<RegisterModel>> getRegisteries() async {
+    return await dataProvider.getAllRegisters();
+  }
+
+  Future<void> updateRegister(RegisterModel register) async {
+    await dataProvider.updateRegistration(register);
+  }
+
+  Future<void> deleteRegister(int reg_id) async {
+    await dataProvider.deleteRegistration(reg_id);
+  }
 }
